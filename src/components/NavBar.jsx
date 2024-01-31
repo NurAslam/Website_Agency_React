b2ed7a50648acc81cc4021c1697a41fcaccc724c
Nur Aslam <aslam21815@gmail.com>
2024-01-31 18:49:03 +0700

    navbar done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -38,8 +38,8 @@ const NavBar = () => {
     ];
     return (
         <header className='w-full bg-white md:bg-transparent fixed top-0 left-0 right-0'>
-            <nav>
-                <div>
+            <nav className={`py-4 lg:px-14 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-white duration-300" : ""}`}>
+                <div className="flex justify-between items-center text-base gap-8">
                     <a href="" className="text-2x1 font-bold flex item-center space-x-3"><img src={logo} alt="" className="w-10 inline-block items-center" /><span className="text-[#263238]">NEXCENT</span></a>
 
                     {/* nav item for large devices */}
@@ -56,12 +56,23 @@ const NavBar = () => {
                     </div>
 
                     <div className="md:hidden">
-                        <button className="focus:outline-none focus:text-gray-500">
+                        <button
+                            onClick={toogleMenu}
+                            className="focus:outline-none focus:text-gray-500">
+
                             {
                                 isMenuOpen ? (<FaXmark className="h-6 w-6" text-netralDGrey />) : (<FaBars className="h-6 w-6 text-netralDGrey" />)
                             }
                         </button>
                     </div>
+                </div>
+
+                {/* Nav item for mobile devices */}
+                <div className={`space-y-4 px-4 mt-16 py-7 bg-brandPrimary`}>
+
+                    {
+                        navItems.map(({ link, path }) => <Link to={path} spy={true} smooth={true} offset={-100} key={path} className="block text-base text-gray-900 hover:text-brandPrimary first:font-medium">{link}</Link>)
+                    }
                 </div>
             </nav>
         </header>
